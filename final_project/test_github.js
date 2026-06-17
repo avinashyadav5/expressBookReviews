1c12000e84f5ec3922f35d2a9303cb675b7633ea
Avinash Yadav <aviydv7518@gmail.com>
2026-06-17 06:20:26 +0530

    Update githubrepo file with correct fork parent info

--- final_project/test_github.js
+++ final_project/test_github.js
@@ -1,20 +1,19 @@
 const fs = require('fs');
 const axios = require('axios');
 
-async function runTest() {
-    let output = `curl -s https://api.github.com/repos/avinashyadav5/expressBookReviews\n`;
+async function run() {
+    const cmd = `curl -s https://api.github.com/repos/avinashyadav5/expressBookReviews | jq '.parent.full_name'`;
+    let output = `${cmd}\n`;
     try {
         const response = await axios.get('https://api.github.com/repos/avinashyadav5/expressBookReviews');
-        output += JSON.stringify(response.data) + '\n';
+        const parentFullName = response.data.parent ? response.data.parent.full_name : null;
+        output += JSON.stringify(parentFullName) + '\n';
     } catch (err) {
-        if (err.response) {
-            output += JSON.stringify(err.response.data) + '\n';
-        } else {
-            output += err.message + '\n';
-        }
+        output += err.message + '\n';
     }
     fs.writeFileSync('githubrepo', output);
-    console.log(`Created githubrepo`);
+    console.log('Created githubrepo');
+    console.log(output);
 }
 
-runTest();
+run();
